9b37b630fb5205c7133577de6bccc0a9572c0f7a
wildRaccoon <chukvm@gmail.com>
2019-02-19 15:42:34 +0200

    add services template

---
+++ WA.Account.Service/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WA.Account.AccountService;
+using WA.Account.Contracts.CheckToken;
+using WA.Account.Contracts.Login;
+
+namespace WA.Account.Service
+{
+    public class AccountService : IAccountService
+    {
+        public CheckTokenResponse CheckToken(CheckTokenRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LoginResponse Login(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
